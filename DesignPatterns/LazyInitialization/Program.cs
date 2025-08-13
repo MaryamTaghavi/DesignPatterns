0ff33d1f4aa66e5b7c0a79651de0731847a51fd5
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-13 10:13:27 +0330

    createLazyInitialization

---
+++ DesignPatterns/LazyInitialization/Program.cs
@@ -0,0 +1,13 @@
+﻿
+public sealed class Singleton
+{
+    private static readonly Lazy<Singleton> _instance =
+        new Lazy<Singleton>(() => new Singleton());
+
+    private Singleton()
+    {
+        Console.WriteLine("Singleton instance created!");
+    }
+
+    public static Singleton Instance => _instance.Value;
+}
