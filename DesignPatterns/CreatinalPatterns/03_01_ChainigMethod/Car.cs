81b378c629a56fe780c5d63da55e8bf006bb773d
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-20 09:01:15 +0330

    createChainingMethodExample

---
+++ DesignPatterns/CreatinalPatterns/03_01_ChainigMethod/Car.cs
@@ -0,0 +1,42 @@
+﻿namespace _03_01_ChainigMethod;
+
+class Car : IBuilder
+{
+    private string brandName;
+    private Product product;
+    public Car(string brand)
+    {
+        product = new Product();
+        this.brandName = brand;
+    }
+    public IBuilder StartUpOperations(string optionalStartUpMessage = " Making a car for you.")
+    { // Starting with brandname
+        product.Add(optionalStartUpMessage);
+        product.Add($"Car model name :{this.brandName}");
+        return this;
+    }
+    public IBuilder BuildBody(string optionalBodyType = "Steel")
+    {
+        product.Add(optionalBodyType);
+        return this;
+    }
+    public IBuilder InsertWheels(int optionalNoOfWheels = 4)
+    {
+        product.Add($"{optionalNoOfWheels} wheels are added");
+        return this;
+    }
+    public IBuilder AddHeadlights(int optionalNoOfHeadLights = 2)
+    {
+        product.Add($"{optionalNoOfHeadLights} Headlights are added");
+        return this;
+    }
+    public IBuilder EndOperations(string optionalEndMessage = "Car construction is completed.")
+    {
+        product.Add(optionalEndMessage);
+        return this;
+    }
+    public Product ConstructCar()
+    {
+        return product;
+    }
+}
---
+++ DesignPatterns/CreatinalPatterns/03_01_ChainigMethod/IBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace _03_01_ChainigMethod;
+
+interface IBuilder
+{
+    IBuilder StartUpOperations(string optionalStartUpMessage =" Making a car for you.");
+    IBuilder BuildBody(string optionalBodyType = "This is a body of a Car");
+    IBuilder InsertWheels(int optionalNoOfWheels = 4);
+    IBuilder AddHeadlights(int optionalNoOfHeadLights = 2);
+    IBuilder EndOperations(string optionalEndMessage = "Car construction is completed.");
+    Product ConstructCar();
+}
