da5187a44e04a390990a4e477c900d3b06dccfef
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-22 12:20:46 +0330

    createAllClasses

---
+++ DesignPatterns/CreatinalPatterns/04_FactoryMethodPattern/Dog.cs
@@ -0,0 +1,9 @@
+﻿namespace _04_FactoryMethodPattern;
+
+public class Dog : IAnimal
+{
+    public void AboutMe()
+    {
+        Console.WriteLine("The dog says: bow-Wow. I prefer barking.");
+    }
+}
