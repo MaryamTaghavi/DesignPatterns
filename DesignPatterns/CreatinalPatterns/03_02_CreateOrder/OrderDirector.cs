999833a82c28639a6f3992e21b3295a33a14eb6a
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-08-29 14:54:14 +0330

    createDirector

---
+++ DesignPatterns/CreatinalPatterns/03_02_CreateOrder/OrderDirector.cs
@@ -0,0 +1,20 @@
+﻿namespace _03_02_CreateOrder;
+
+public class OrderDirector
+{
+    private readonly IOrder _builder;
+
+    public OrderDirector(IOrder builder)
+    {
+        _builder = builder;
+    }
+
+    public Order Build()
+    {
+        return _builder
+            .CreateAddress()
+            .CreateDiscount()
+            .CreatePayment()
+            .CreateOrder();
+    }
+}
--- DesignPatterns/CreatinalPatterns/03_02_CreateOrder/Program.cs
+++ DesignPatterns/CreatinalPatterns/03_02_CreateOrder/Program.cs
@@ -3,3 +3,14 @@
 چطور با Builder Pattern می‌تونی این Order رو مرحله به مرحله بسازی بدون اینکه Constructor بزرگ و پیچیده بسازی؟
 */
 
+/*
+پترن بیلدر هم میتواند با دایرکتور که مراحل ساخت را مشخص کند ساخته میشود
+هم بدون آن.
+ */
+
+using _03_02_CreateOrder;
+
+var director = new OrderDirector(new FirstOrder());
+Order order = director.Build();
+
+Console.ReadLine();
