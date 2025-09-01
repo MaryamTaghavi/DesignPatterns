b013b0e87765085708ef18a5d7af140209ff7f00
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-09-01 21:55:38 +0330

    addDescriptionToDecorate

--- DesignPatterns/StructuralPatterns/02_00_DecoratorPattern/Program.cs
+++ DesignPatterns/StructuralPatterns/02_00_DecoratorPattern/Program.cs
@@ -4,48 +4,71 @@
 به طور خلاصه بتوان فیچر جدید بدون دست زدن به کدهای قبلی اضافه کرد.
  */
 
+/*
+مثالی از این پترن یعنی اینکه یک خونه داریم و داریم دائما بهش فیچر اضافه میکنیم
+بدون این که اسکلت خونه تغییر کنه
+یعنی یه سقف دیگه اضافه میکنیم
+رنگش میکنیم و ...
+*/
+
 using _02_00_DecoratorPattern;
 
 Console.WriteLine("***Decorator pattern Demo***\n");
 
 #region Scenario-1
-Console.WriteLine("\n**Scenario-1:");
-Console.WriteLine("**Building home.Adding floor and then painting it.* *");
-AbstractHome home = new ConcreteHome();
-Console.WriteLine("Current bill breakups are as follows:");
-home.MakeHome();
+
+    Console.WriteLine("\n**Scenario-1:");
+    Console.WriteLine("**Building home.Adding floor and then painting it.* *");
+    AbstractHome home = new ConcreteHome();
+    Console.WriteLine("Current bill breakups are as follows:");
+    home.MakeHome();
+
 // Applying a decorator
 // Adding a floor
-home = new FloorDecorator(home);
-Console.WriteLine("\nFloor added.Current bill breakups are as follows: ");
-home.MakeHome();
+
+    home = new FloorDecorator(home);
+    Console.WriteLine("\nFloor added.Current bill breakups are as follows: ");
+    home.MakeHome();
+
 // Working on top of the previous decorator.
 // Painting the home
-home = new PaintDecorator(home);
 
-Console.WriteLine("\nPaint applied.Current bill breakups are as follows: ");
-home.MakeHome();
+    home = new PaintDecorator(home);
+
+    Console.WriteLine("\nPaint applied.Current bill breakups are as follows: ");
+    home.MakeHome();
+
 #endregion
 
 #region Scenario-2
-Console.WriteLine("\n**Scenario-2:");
-Console.WriteLine("**Building home,painting it and then adding two additional floors on top of it.* *");
+
+    Console.WriteLine("\n**Scenario-2:");
+    Console.WriteLine("**Building home,painting it and then adding two additional floors on top of it.* *");
+
 // Fresh start once again.
-home = new ConcreteHome();
-Console.WriteLine("\nGoing back to original home.Current bill breakups are as follows:");
-home.MakeHome();
+
+    home = new ConcreteHome();
+    Console.WriteLine("\nGoing back to original home.Current bill breakups are as follows:");
+    home.MakeHome();
+
 // Applying paint on original home.
-home = new PaintDecorator(home);
-Console.WriteLine("\nPaint applied.Current bill breakups are as follows: ");
-home.MakeHome();
+
+    home = new PaintDecorator(home);
+    Console.WriteLine("\nPaint applied.Current bill breakups are as follows: ");
+    home.MakeHome();
+
 // Adding a floor on the painted home.
-home = new FloorDecorator(home);
-Console.WriteLine("\nFloor added.Current bill breakups are as follows: ");
-home.MakeHome();
+
+    home = new FloorDecorator(home);
+    Console.WriteLine("\nFloor added.Current bill breakups are as follows: ");
+    home.MakeHome();
+
 // Adding another floor on the current home.
-home = new FloorDecorator(home);
-Console.WriteLine("\nFloor added.Current bill breakups are as follows: ");
-home.MakeHome();
+
+    home = new FloorDecorator(home);
+    Console.WriteLine("\nFloor added.Current bill breakups are as follows: ");
+    home.MakeHome();
+
 #endregion
 
 Console.ReadKey();
