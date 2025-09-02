136fdf83285d4acda690cda6e52fd4ef8501fcf9
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-09-02 18:42:50 +0330

    completeDemonstration

--- DesignPatterns/StructuralPatterns/03_01_Demonstration1/Program.cs
+++ DesignPatterns/StructuralPatterns/03_01_Demonstration1/Program.cs
@@ -8,21 +8,24 @@ using _03_01_Demonstration1;
 
 Console.WriteLine("***Adapter Pattern Demo***\n");
 IRectangle rectangle = new Rectangle(20, 10);
+
 Console.WriteLine("For initial verification purposes, printing the areas of both shapes.");
-Console.WriteLine("Rectangle area is:{0} Square unit",
-rectangle.CalculateArea());
+Console.WriteLine("Rectangle area is:{0} Square unit", rectangle.CalculateArea());
+
 ITriangle triangle = new Triangle(20, 10);
-Console.WriteLine("Triangle area is:{0} Square unit", triangle.
-CalculateAreaOfTriangle());
+Console.WriteLine("Triangle area is:{0} Square unit", triangle.CalculateAreaOfTriangle());
 Console.WriteLine("\nNow using the adapter.");
+
 IRectangle adapter = new RectangleAdapter(triangle);
 Console.Write("True fact : ");
 adapter.AboutMe();
+
 Console.WriteLine($" and my area is : {adapter.CalculateArea()} square unit.");
 // Alternative way:
 Console.WriteLine("\nUsing the adapter in a different way now.");
 // Passing a Triangle instead of a Rectangle
 Console.WriteLine($"Area of the triangle using the adapter is :{GetDetails(adapter)} square unit.");
+
 Console.ReadKey();
 
 /*
