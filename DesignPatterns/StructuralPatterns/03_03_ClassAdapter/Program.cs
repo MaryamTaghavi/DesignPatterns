34bd1ac99323f2b0cf237422d99b2986f2492271
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-09-03 08:39:26 +0330

    addTemplateForClassAdapter

---
+++ DesignPatterns/StructuralPatterns/03_03_ClassAdapter/Program.cs
@@ -0,0 +1,29 @@
+﻿
+OldLamp oldLamp = new OldLamp();
+oldLamp.TurnOn("old");
+
+INewLamp newLamp = new LampAdapter();
+newLamp.TurnOnNewLamp("new");
+
+Console.ReadLine();
+
+public class OldLamp
+{
+    public void TurnOn(string lamp)
+    {
+        Console.WriteLine("TurnOn {0}" , lamp);
+    }
+}
+
+public interface INewLamp
+{
+    void TurnOnNewLamp(string lamp);
+}
+
+public class LampAdapter : OldLamp, INewLamp
+{
+    public void TurnOnNewLamp(string newLamp)
+    {
+        TurnOn(newLamp);
+    }
+}
