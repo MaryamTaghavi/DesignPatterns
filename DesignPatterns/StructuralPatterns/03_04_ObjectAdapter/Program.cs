eeb936f241291df33063085b3e508c731f638526
MaryamTaghavi <m.taghavi@megagroup.biz>
2025-09-03 09:20:02 +0330

    createSamplePrinterAdapter

---
+++ DesignPatterns/StructuralPatterns/03_04_ObjectAdapter/Program.cs
@@ -0,0 +1,33 @@
+﻿
+OldPrinter oldPrinter = new OldPrinter();
+oldPrinter.Print("old");
+
+INewPrinter newPrinter = new PrinterAdapter(oldPrinter);
+newPrinter.Print("new");
+
+public class OldPrinter
+{
+    public void Print(string printerName)
+    {
+        Console.WriteLine("Please use {0}" , printerName);
+    }
+}
+
+public interface INewPrinter
+{
+    void Print(string printerName);
+}
+
+public class PrinterAdapter : INewPrinter
+{
+    OldPrinter _oldPrinter;
+    public PrinterAdapter(OldPrinter oldPrinter)
+    {
+        _oldPrinter = oldPrinter;
+    }
+
+    public void Print(string printerName)
+    {
+        _oldPrinter.Print(printerName);
+    }
+}
