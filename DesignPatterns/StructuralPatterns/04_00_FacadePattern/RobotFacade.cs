6f5d0a7484d8c74bc1ffe46213c6e50cb903e702
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-06 13:11:24 +0330

    createRobotFacade

---
+++ DesignPatterns/StructuralPatterns/04_00_FacadePattern/RobotFacade.cs
@@ -0,0 +1,29 @@
+﻿namespace _04_00_FacadePattern;
+
+public class RobotFacade
+{
+    RobotBody robotBody;
+    RobotColor robotColor;
+
+    public RobotFacade(string robotType , string color = "steel")
+    {
+        robotBody = new RobotBody(robotType);
+        robotColor = new RobotColor(color);
+    }
+
+    public void CostructRobot()
+    {
+        Console.WriteLine("Robot creation through facade starts ...");
+        robotBody.MakeRobotBody();
+        robotColor.SetColor();
+        Console.WriteLine();
+    }
+
+    public void DestroyRobot()
+    {
+        Console.WriteLine("Making an attempt to destroy one robot using the facade now.");
+        robotColor.RemoveColor();
+        robotBody.DestroyRobotBody();
+        Console.WriteLine();
+    }
+}
