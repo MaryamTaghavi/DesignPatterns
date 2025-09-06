3417bb4484e424890a00a13c01ce428410460139
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-06 13:40:58 +0330

    createPaymentClass

---
+++ DesignPatterns/StructuralPatterns/04_01_OrderRequest/Inventory.cs
@@ -0,0 +1,14 @@
+﻿namespace _04_01_OrderRequest;
+
+public class Inventory
+{
+    public bool CheckInventory(string itemId)
+    {
+        return true;
+    }
+
+    public void ReduceInventory(string itemId , int amount)
+    {
+        Console.WriteLine("Reducing inventory of " + itemId + " by " + amount);
+    }
+}
---
+++ DesignPatterns/StructuralPatterns/04_01_OrderRequest/Payment.cs
@@ -0,0 +1,20 @@
+﻿namespace _04_01_OrderRequest;
+
+public class Payment
+{
+    private readonly string _name;
+    private readonly string _cardNumber;
+    private readonly float _amount;
+
+    public Payment(string name, string cardNumber, float amount)
+    {
+        _name = name;
+        _cardNumber = cardNumber;
+        _amount = amount;
+    }
+
+    public void Pay()
+    {
+        Console.WriteLine("Charging card with name " + _name);
+    }
+}
