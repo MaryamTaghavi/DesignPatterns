461af4c276e9ca9732f080a583b3250e045cfe67
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-06 13:28:11 +0330

    createOrderRequest

---
+++ DesignPatterns/StructuralPatterns/04_01_OrderRequest/OrderRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace _04_01_OrderRequest;
+
+public class OrderRequest
+{
+    public string Name { get; } = "danny";
+    public string CardNumber { get; } = "1234";
+    public float Amount { get; } = 20.99f;
+    public string Address { get; } = "123 springfield way, Texas";
+    public string[] ItemIds { get; } = { "123" , "456" , "789"};
+}
