2d5d37246de513d686f81f7020068ee8b7107a71
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-06 12:56:08 +0330

    createRobotClasses

---
+++ DesignPatterns/StructuralPatterns/04_00_FacadePattern/RobotBody.cs
@@ -0,0 +1,33 @@
+﻿namespace _04_00_FacadePattern;
+
+public class RobotBody
+{
+    string robotType;
+    static int count = 0;
+
+    public RobotBody(string robotType)
+    {
+        this.robotType = robotType ;
+    }
+
+    public void MakeRobotBody()
+    {
+        Console.WriteLine($"Constructing one {robotType} robot.");
+        Console.WriteLine($"Robot creatin finished.");
+        Console.WriteLine($"Total number of robot created at this moment={++count}");
+    }
+
+    public void DestroyRobotBody()
+    {
+        if (count > 0)
+        {
+            --count;
+            Console.WriteLine("Robot's destruction process is over.");
+        }
+        else
+        {
+            Console.WriteLine("All robots are destroyed.");
+            Console.WriteLine("Color removal operation will not continue.");
+        }
+    }
+}
---
+++ DesignPatterns/StructuralPatterns/04_00_FacadePattern/RobotColor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_00_FacadePattern
+{
+    internal class RobotColor
+    {
+    }
+}
