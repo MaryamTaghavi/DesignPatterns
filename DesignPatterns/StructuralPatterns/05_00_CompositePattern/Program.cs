959bd5ff52a6ebaa41b096c901e1c057bc475094
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-07 12:10:12 +0330

    addDescToCompositePattern

--- DesignPatterns/StructuralPatterns/05_00_CompositePattern/Program.cs
+++ DesignPatterns/StructuralPatterns/05_00_CompositePattern/Program.cs
@@ -1,18 +1,24 @@
 ﻿using _05_00_CompositePattern;
 
 /*
+Composite به معنای مرکب است.
+*/
+
+/*
 مزایای این پترن : یکنواختی (uniformity) است و میتوان با یک اینترفیس به کل ساختار دسترسی داشت.
 انعلاف پذیری و مدیریت سلسله مراتب ها
- */
+*/
 
 /*
 معایب این پترن :
 پیچیدگی مدیریت فرزندان : ordering آنها،
 افزایش هزینه نگهداری نودها، امکان نقض کنترل دسترسی (چون همه نودها از یک اینترفیس پیروی میکنند، ممکنه عملیاتی به انها تحمیل شود که معنادار نیست)
- */
-Console.WriteLine("***Composite Pattern Demo. ***");
+*/
+
+Console.WriteLine("*** Composite Pattern Demo. ***");
 
 #region Mathematics department
+
 // 2 lecturers work in Mathematics department
 Employee mathTeacher1 = new Employee
 {
@@ -36,9 +42,11 @@ CompositeEmployee hodMaths = new CompositeEmployee
 // Lecturers of Mathematics directly reports to HOD-Maths
 hodMaths.AddEmployee(mathTeacher1);
 hodMaths.AddEmployee(mathTeacher2);
+
 #endregion
 
 #region Computer Science department
+
 // 3 lecturers work in Computer Sc. department
 Employee cseTeacher1 = new Employee
 {
@@ -69,9 +77,11 @@ CompositeEmployee hodCompSc = new CompositeEmployee
 hodCompSc.AddEmployee(cseTeacher1);
 hodCompSc.AddEmployee(cseTeacher2);
 hodCompSc.AddEmployee(cseTeacher3);
+
 #endregion
 
 #region Top level management
+
 // The college also has a Principal
 CompositeEmployee principal = new CompositeEmployee
 {
@@ -83,27 +93,37 @@ CompositeEmployee principal = new CompositeEmployee
 reports to Principal.*/
 principal.AddEmployee(hodMaths);
 principal.AddEmployee(hodCompSc);
+
 #endregion
 
 /*
 * Printing the leaf-nodes and branches in the same way. i.e.
 in each case, we are calling DisplayDetails() method.
 */
+
 Console.WriteLine("\nDetails of a Principal object is as follows:");
+
 // Prints the complete structure
+
 principal.DisplayDetails();
 Console.WriteLine("\nDetails of a HOD object is as follows:");
+
 /* Prints the details of Computer Science department */
+
 hodCompSc.DisplayDetails();
 // Leaf node
+
 Console.WriteLine("\nDetails of an individual employee(leafnode) is as follows:");
 mathTeacher1.DisplayDetails();
+
 /*
 * Suppose, one Computer Science lecturer(C.Jones)
 * is leaving now from the organization.
 */
+
 hodCompSc.RemoveEmployee(cseTeacher2);
 Console.WriteLine("\nAfter the resignation of C.Jones, the organization has the following members: ");
 principal.DisplayDetails();
 // Wait for user
+
 Console.ReadKey();
