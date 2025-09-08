d0f8745f48071b31380cfa29c8baf5fa3051acf7
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-08 12:41:48 +0330

    completecClientCode

--- DesignPatterns/StructuralPatterns/06_00_BridgePattern/Program.cs
+++ DesignPatterns/StructuralPatterns/06_00_BridgePattern/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// This is CConsole.WriteLine("***Bridge Pattern Demo.***");
+
+using _06_00_BridgePattern;
+
+Console.WriteLine("Verifying the market price of a television.");
+ElectronicGoods eItem = new Television();
+eItem.ProductType = "Sony Television";
+// Verifying online price
+IPrice price = new OnlinePrice();
+eItem.Price = price;
+
+eItem.Details();
+// Verifying showroom price
+price = new ShowroomPrice();
+eItem.Price = price;
+eItem.Details();
