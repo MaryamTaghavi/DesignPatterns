5dbd1112d2167cfd69a9e6ef8be1b62fecaf1302
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-08 13:43:38 +0330

    createRemoteControlProject

---
+++ DesignPatterns/StructuralPatterns/06_01_RemoteControl/RemoteControl.cs
@@ -0,0 +1,9 @@
+﻿namespace _06_01_RemoteControl;
+
+public abstract class RemoteControl
+{
+    public abstract void TurnOn();
+    public abstract void TurnOff();
+    public abstract void VolumeUp();
+    public abstract void VolumeDown();
+}
