2e402d798d8cf753936eb39a4df1d473f9cf8c07
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-12 17:19:45 +0330

    createIVisitor

---
+++ DesignPatterns/BehavioralPatterns/01_01_CombineComposite&Visitor/IVisitor.cs
@@ -0,0 +1,10 @@
+﻿namespace _01_01_CombineComposite_Visitor;
+
+public interface IVisitor
+{
+    // leaf nodes
+    void VisitEmployees(Employee  employee);
+
+    // composite nodes
+    void VisitEmployee(CompositeEmployee employee);
+}
---
+++ DesignPatterns/BehavioralPatterns/01_01_CombineComposite&Visitor/PromotionCheckerVisitor.cs
@@ -0,0 +1,15 @@
+﻿namespace _01_01_CombineComposite_Visitor;
+
+// Concrete visitor class
+public class PromotionCheckerVisitor : IVisitor
+{
+    public void VisitEmployee(CompositeEmployee employee)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void VisitEmployees(Employee employee)
+    {
+        throw new NotImplementedException();
+    }
+}
