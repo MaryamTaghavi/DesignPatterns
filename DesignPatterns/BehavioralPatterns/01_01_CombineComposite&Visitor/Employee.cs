5f218b3a520894b661c8334be302103a5aad4c3c
MaryamTaghavi <m.taghavi.ce@gmail.com>
2025-09-12 17:27:05 +0330

    addEmployee

---
+++ DesignPatterns/BehavioralPatterns/01_01_CombineComposite&Visitor/Employee.cs
@@ -0,0 +1,19 @@
+﻿namespace _01_01_CombineComposite_Visitor;
+
+public class Employee : IEmployee
+{
+    public string Name { get; set; }
+    public string Dept { get; set; }
+    public string Designation { get; set; }
+    public double Experience { get; set; }
+
+    public void Accept(IVisitor visitor)
+    {
+        visitor.VisitEmployees(this);
+    }
+
+    public void DisplayDetails()
+    {
+        Console.WriteLine($"{Name} works in {Dept} department. Designation {Designation}. Experience : {Experience} years." );
+    }
+}
---
+++ DesignPatterns/BehavioralPatterns/01_01_CombineComposite&Visitor/IEmployee.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_01_CombineComposite_Visitor;
+
+public interface IEmployee
+{
+    string Name { get; set; }
+    string Dept { get; set; }
+    string Designation { get; set; }
+    void DisplayDetails();
+    double Experience { get; set; }
+    void Accept(IVisitor visitor);
+}
